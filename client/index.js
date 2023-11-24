09249efa71b058b256a69e03f06b8c67a8fc5538
wyang0917 <yangwang0917@gmail.com>
2023-11-24 11:06:54 +1000

    highline finsh

--- client/index.js
+++ client/index.js
@@ -1,1 +1,23 @@
 import "./index.css";
+
+import { highline } from "@/js/database";
+
+
+const middle = document.querySelector('.main-middle')
+const middle_highline = middle.querySelector('.highline')
+const middle_highline_description = middle_highline.querySelectorAll('.description')
+const middle_highline_description_img = middle_highline.querySelectorAll('.description img')
+const middle_highline_description_p = middle_highline.querySelectorAll('.description p')
+
+
+middle_highline_description_p.forEach((ele,i)=>{
+  ele.textContent = highline.description[i]
+})
+//background image
+middle_highline_description.forEach((ele,i)=>{
+  ele.style.backgroundImage=`url("./images/highline/highline_${i+1}.jpeg")`
+})
+//profile img
+middle_highline_description_img.forEach((ele,i)=>{
+  ele.src=`./images/profile/profile_${i+1}.png`
+})
---
+++ client/src/js/database.js
@@ -0,0 +1,21 @@
+//third party import
+import axios from 'axios'
+
+//file import
+import { getPath } from './utiles'
+
+async function getHighline(){
+  const path = getPath('highline');
+  const response = await getInfo(path)
+  console.log(response);
+  return response.data
+} 
+async function getInfo(path){
+  try{
+    return await axios.get(path)
+  }catch(error){
+    console.error(error);
+  }
+}
+
+export const highline = await getHighline()
